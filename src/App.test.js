3c702816abaa699527a4bf73b45d2c19cf7196ae
trevorjohn3202-rgb <trevorjohn3202@gmail.com>
2026-06-24 15:15:05 +0300

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Miraj page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getAllByText(/Miraj/i)[0];
   expect(linkElement).toBeInTheDocument();
+
 });
+
